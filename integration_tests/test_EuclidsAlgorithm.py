669a7808a5caa514ab8ad36b0b8b6e49b141a973
Thas-Tayapongsak <hwunhwun@gmail.com>
2025-12-21 14:45:55 +0700

    fix run_app argument in test_EuclidsAlgorithm

--- integration_tests/test_EuclidsAlgorithm.py
+++ integration_tests/test_EuclidsAlgorithm.py
@@ -1,10 +1,10 @@
 def test_gcd_calculation(run_app):
-    result = run_app("EuclidsAlgorithm", ["48", "18"])
+    result = run_app("EuclidsAlgorithm", args=["48", "18"])
 
     assert result.returncode == 0
     assert "The greated common divisor of 48 and 18 is 6" in result.stdout
 
 def test_missing_args(run_app):
-    result = run_app("EuclidsAlgorithm", ["20"])
+    result = run_app("EuclidsAlgorithm", args=["20"])
 
     assert result.returncode == 1
